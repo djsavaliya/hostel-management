eab5bb5fea818ca7f856c9bc386724ea5fa1070c
djsavaliya <42993834+djsavaliya@users.noreply.github.com>
2020-04-23 02:56:19 +0530

    Add files via upload

---
+++ Managers/templates/header.php
@@ -0,0 +1,43 @@
+<head>
+	<title>HMS</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<style type="text/css">
+		.brand{
+			background: #cbb09c !important;
+		}
+		.brand-text{
+			color: #cbb09c !important;
+			font-size: 40px !important;
+		}
+		.title-text{
+			font-size: 25px;
+			text-align: center;
+		}
+		.title-box{
+			margin: 0px 25px;
+		}
+		form{
+			max-width: 460px;
+			margin: 20px auto;
+			padding: 20px;
+		}
+	</style>
+</head>
+<body class="grey lighten-2">
+	<nav class="white z-depth-0">
+		<div class="container row">
+			<a href="index.php" class="brand-logo brand-text">HOSTEL</a>
+			<div class="col s1 push-s7 title-box">
+				<span class="flow-text"><a href="http://localhost/amoc/managers/index.php" class="green-text text-lighten-3 z-depth-0 title-text">Home</a></span>
+			</div>
+			<div class="col s2 push-s7 title-box">
+				<span class="flow-text"><a href="http://localhost/amoc/managers/applications.php" class="green-text text-lighten-3 z-depth-0 title-text">Applications</a></span>
+			</div>
+			<div class="col s1 push-s7 title-box">
+				<span class="flow-text"><a href="http://localhost/amoc/managers/students.php" class="green-text text-lighten-3 z-depth-0 title-text">Students</a></span>
+			</div>
+			<div class="col s1 push-s7 title-box">
+				<span class="flow-text"><a href="http://localhost/amoc/managers/profile.php" class="green-text text-lighten-3 z-depth-0 title-text">Profile</a></span>
+			</div>
+		</div>
+	</nav>
