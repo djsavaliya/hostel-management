ba6761a3538ae4ad275a134fed03595bc192ef88
djsavaliya <42993834+djsavaliya@users.noreply.github.com>
2020-04-23 02:58:24 +0530

    Add files via upload

--- databases/hostels.sql
+++ databases/hostels.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `hostels`;
 
 CREATE TABLE `hostels` (
  `hostel_id` int(11) NOT NULL AUTO_INCREMENT,
- `hoste_name` varchar(255) NOT NULL,
+ `hostel_name` varchar(255) NOT NULL,
  `no_of_rooms` int(11) NOT NULL,
  `no_of_students` int(11) NOT NULL,
  PRIMARY KEY (`hostel_id`)
