9f50a89a005e83706f29a47ddda404b4eb8198c4
djsavaliya <42993834+djsavaliya@users.noreply.github.com>
2020-04-24 01:33:27 +0530

    Add files via upload

--- Managers/applications.php
+++ Managers/applications.php
@@ -1,10 +1,72 @@
-<?php  ?>
+<?php 
+	require 'session.php';
+
+	$sql = 'SELECT * FROM applications,students,hostels WHERE (applications.student_id = students.student_id) AND (applications.hostel_id = hostels.hostel_id)';
+	$result = mysqli_query($conn,$sql);
+	$applications = mysqli_fetch_all($result,MYSQLI_ASSOC);
+
+	$error = '';
+	foreach($applications as $application):
+		//echo 'happenin';
+		if(isset($_POST[$application['student_id']])){
+			$students = $application['no_of_students']+1;
+			$hid = $application['hostel_id'];
+			$sid = $application['student_id'];
+			$aid = $application['application_id'];
+			$sql1 = "UPDATE hostels SET no_of_students = '$students' WHERE hostels.hostel_id = '$hid'";
+			$result1 = mysqli_query($conn,$sql1);
+			//if($result1==true){$error='true';}else{$error='false';}
+			$sql2 = "UPDATE students SET hostel_id = '$hid' WHERE students.student_id = '$sid'";
+			$result2 = mysqli_query($conn,$sql2);
+			//if($result1==true){$error='true';}else{$error='false';}
+			$sql3 = "DELETE FROM applications WHERE applications.application_id = '$aid'";
+			$result3 = mysqli_query($conn,$sql3);
+			//if($result1==true){$error='true';}else{$error='false';}
+			break;
+		}
+		if(isset($_POST[$application['hostel_id']])){
+			$aid = $application['application_id'];
+			$sql4 = "DELETE FROM applications WHERE applications.application_id = '$aid'";
+			$result4 = mysqli_query($conn,$sql4);
+			//if($result1==true){$error='true';}else{$error='false';}
+			break;
+		}
+	endforeach;
+
+	$sql = 'SELECT * FROM applications,students,hostels WHERE (applications.student_id = students.student_id) AND (applications.hostel_id = hostels.hostel_id)';
+	$result = mysqli_query($conn,$sql);
+	$applications = mysqli_fetch_all($result,MYSQLI_ASSOC);
+
+	mysqli_free_result($result);
+	mysqli_close($conn);
+ ?>
 
 <!DOCTYPE HTML>
 <html>
 	<?php include('templates/header.php'); ?>
 
-	
+	<form action="applications.php" method="post">
+		<h4 class="center grey-text">Applications</h4>
+		<div class="container">
+			<div class="row">
+				<div style="color:red;"><?php echo $error; ?></div>
+				<?php foreach($applications as $application):?>
+					<div class="col s12 md3">
+						<div class="card z-depth-0">
+							<div class="card-content center">
+								<h5><?php echo htmlspecialchars($application['first_name'].' '.$application['last_name']);?></h5>
+								<h6><?php echo htmlspecialchars($application['hostel_name'])?></h6>
+								<div class="center">
+									<button type="submit" name="<?php echo htmlspecialchars($application['student_id']); ?>" class="btn brand z-depth-0">Accept</button>
+									<button type="submit" name="<?php echo htmlspecialchars($application['hostel_id']); ?>" class="btn brand z-depth-0">Reject</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				<?php endforeach; ?>
+			</div>
+		</div>
+	</form>
 
 	<?php include('templates/footer.php'); ?>
 </html>
--- Managers/profile.php
+++ Managers/profile.php
@@ -1,23 +1,5 @@
 <?php
-	session_start();// Starting Session
-	// Establishing Connection with Server by passing server_name, user_id and password as a parameter
-	//$conn = mysql_connect("localhost", "root", "");
-	require '../config/db_connect.php';
-	// Selecting Database
-	//$db = mysqli_select_db($conn , "hms");
-	// Storing Session
-	$user_check=$_SESSION['username'];
-	//echo ($user_check);
-	// SQL Query To Fetch Complete Information Of User
-	$sql = "SELECT * FROM managers WHERE email='$user_check'";
-	$result = mysqli_query($conn,$sql);
-	$student = mysqli_fetch_assoc($result);
-	$name = $student['first_name'];
-	if(!isset($name)){
-		//echo 'not set';
-		mysqli_close($conn); // Closing Connection
-		header('Location: index.php'); // Redirecting To Home Page
-	}
+	require 'session.php';
 ?>
 
 
@@ -26,9 +8,9 @@
 	<?php include('templates/header.php'); ?>
 
 	<div class="z-depth-0 center">
-			<h4><?php echo htmlspecialchars($student['first_name']);echo(" ");echo htmlspecialchars($student['last_name']);?></h4>
-			<h5><?php echo htmlspecialchars($student['mobile'])?></h5>
-			<h5><?php echo htmlspecialchars($student['email'])?></h5>
+			<h4><?php echo htmlspecialchars($manager['first_name']);echo(" ");echo htmlspecialchars($manager['last_name']);?></h4>
+			<h5><?php echo htmlspecialchars($manager['mobile'])?></h5>
+			<h5><?php echo htmlspecialchars($manager['email'])?></h5>
 	</div>
 	<div class="center">
 		<a href="../logout.php"><button type="submit" name="logout" class="btn brand z-depth-0">Log Out</button></a>
