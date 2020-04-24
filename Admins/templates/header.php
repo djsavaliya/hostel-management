7a56e60e8b4a8122afed4c2b696ebbf9d0351730
djsavaliya <42993834+djsavaliya@users.noreply.github.com>
2020-04-25 00:20:55 +0530

    Update header.php

--- Admins/templates/header.php
+++ Admins/templates/header.php
@@ -35,7 +35,7 @@
 				<span class="flow-text"><a href="http://localhost/amoc/admins/index.php" class="green-text text-lighten-3 z-depth-0 title-text">Home</a></span>
 			</div>
 			<div class="col s1 push-s7 title-box">
-				<span class="flow-text"><a href="http://localhost/amoc/admins/applications.php" class="green-text text-lighten-3 z-depth-0 title-text">Hostels</a></span>
+				<span class="flow-text"><a href="http://localhost/amoc/admins/hostels.php" class="green-text text-lighten-3 z-depth-0 title-text">Hostels</a></span>
 			</div>
 			<div class="col s1 push-s7 title-box">
 				<span class="flow-text"><a href="http://localhost/amoc/admins/students.php" class="green-text text-lighten-3 z-depth-0 title-text">Students</a></span>
