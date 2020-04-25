ef5ece26a9db8b193b7243d2580e5582a9be2163
Jay Chawla <44499984+chawlajay@users.noreply.github.com>
2020-04-25 23:23:02 +0530

    added footer css

--- Students/templates/header.php
+++ Students/templates/header.php
@@ -21,23 +21,32 @@
 			margin: 20px auto;
 			padding: 20px;
 		}
+		footer{
+			position: fixed;
+			left: 0;
+			bottom: 0;
+			width: 100%;
+			background-color: #ffff99;
+			color: white;
+			text-align: center;
+		}
 	</style>
 </head>
 <body class="grey lighten-2">
 	<nav class="white z-depth-0">
 		<div class="container row">
-			<a href="index.php" class="brand-logo brand-text">HOSTEL</a>
+		<a href="index.php" class="brand-logo green-text">HOSTEL MANAGEMENT SYSTEM</a>
 			<div class="col s1 push-s7 title-box">
-				<span class="flow-text"><a href="http://localhost/amoc/students/index.php" class="green-text text-lighten-3 z-depth-0 title-text">Home</a></span>
+				<span class="flow-text blue-text"><a href="http://localhost/amoc/students/index.php" class="green-text text-lighten-3 z-depth-0 title-text">Home</a></span>
 			</div>
 			<div class="col s1 push-s7 title-box">
-				<span class="flow-text"><a href="http://localhost/amoc/students/apply.php" class="green-text text-lighten-3 z-depth-0 title-text">Apply</a></span>
+				<span class="flow-text blue-text"><a href="http://localhost/amoc/students/apply.php" class="green-text text-lighten-3 z-depth-0 title-text">Apply</a></span>
 			</div>
 			<div class="col s1 push-s7 title-box">
-				<span class="flow-text"><a href="http://localhost/amoc/students/profile.php" class="green-text text-lighten-3 z-depth-0 title-text">Profile</a></span>
+				<span class="flow-text blue-text"><a href="http://localhost/amoc/students/profile.php" class="green-text text-lighten-3 z-depth-0 title-text">Profile</a></span>
 			</div>
 			<div class="col s1 push-s7 title-box">
-				<span class="flow-text"><a href="http://localhost/amoc/students/contact.php" class="green-text text-lighten-3 z-depth-0 title-text">Contact</a></span>
+				<span class="flow-text blue-text"><a href="http://localhost/amoc/students/contact.php" class="green-text text-lighten-3 z-depth-0 title-text">Contact</a></span>
 			</div>
 		</div>
 	</nav>
