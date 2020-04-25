e747ef43386a819d7c2a8da445228d609cf8f38c
Jay Chawla <44499984+chawlajay@users.noreply.github.com>
2020-04-25 21:29:43 +0530

    created header.php for signup and signin pages
    this header is used in Student signup and student signin pages , also in Manager/Admin Signin page.

---
+++ templates/header.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<style type="text/css">
+		.brand{
+			background: #cbb09c !important;
+		}
+		.brand-text{
+			color: #cbb09c !important;
+			font-size: 40px !important;
+		}
+		.title-text{
+			font-size: 25px;
+			text-align: center;
+		}
+		.title-box{
+			margin: 0px 25px;
+		}
+		form{
+			max-width: 460px;
+			margin: 20px auto;
+			padding: 20px;
+		}
+		table,tr,td{
+			border:1px solid black;
+			text-align: center;
+		}
+	</style>
+</head>
+<body class="grey lighten-2">
+	<nav class="white z-depth-0">
+		<div class="container row">
+			<a href="index.php" class="brand-logo green-text text-lighten-2 center">HOSTEL MANAGEMENT SYSTEM</a>
+			
+		</div>
+	</nav>
+</html>
